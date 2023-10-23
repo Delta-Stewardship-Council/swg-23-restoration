6a1b2daedbf1c6f6d551734659294a1182c6ff59
Goertler <pascale.goertler@water.ca.gov>
2023-10-23 11:11:23 -0700

    test tidytext on Delta Conservancy pdf examples

---
+++ R/test_tidytext.R
@@ -0,0 +1,108 @@
+### test text extraction from pdfs with initial docs from Delta Conservancy
+### compare word differences in each type of doc
+### Pascale 10/23/23
+
+# library
+library(pdftools)
+library(readtext)
+library(tidytext)
+library(dplyr)
+library(ggplot2)
+library(tm)
+library(ggpubr)
+
+# upload pdf
+dir <- "data_raw" # names location of pdf files
+files <- list.files(dir, pattern = "*.pdf$", full.names = TRUE) # creates vector of pdf file names
+
+# get data (in Plain Text UTF-8)
+tidy_pdf <- readtext(files)
+
+# make text data
+tidy_words <- tidy_pdf %>%
+  unnest_tokens(word, text)
+
+# remove stop words
+tidy_stop_words <- tidy_words %>%
+  anti_join(stop_words) # without stop words (reduced by 17,591)
+
+# remove numbers and symbols
+tidy_stop_num_words <- mutate(tidy_stop_words, word = gsub(x = word, pattern = "[0-9]+|[[:punct:]]|\\(.*\\)", replacement = ""))
+
+tidy_stop_num_words <-  tidy_stop_num_words[!(is.na(tidy_stop_num_words$word) | tidy_stop_num_words$word ==""), ] # reduced by another 3,233
+
+# check
+check <- tidy_stop_num_words %>%
+  count(word, sort = TRUE)
+
+check  %>%
+  filter(n > 50) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col(fill = "darkred") +
+  xlab(NULL) +
+  ylab("Word Count") +
+  coord_flip() +
+  ggtitle("Word Usage")
+
+# look at each pdf separately and compare top words
+staff_rec <- tidy_stop_num_words %>%
+  filter(doc_id == "AI 8.6 Prop 1-1605_Staff Recommendation_final.pdf") %>%
+  count(word, sort = TRUE) # 613
+
+proposal <- tidy_stop_num_words %>%
+  filter(doc_id == "P1-1605_Full Proposal.pdf") %>%
+  count(word, sort = TRUE) # 2194
+
+report <- tidy_stop_num_words %>%
+  filter(doc_id == "P1-1605_ProgressReport_P1-1605_Deliv 1g_Final Report.pdf") %>%
+  count(word, sort = TRUE) # 835
+
+contract <- tidy_stop_num_words %>%
+  filter(doc_id == "Prop 1-1605 A2_Fully Executed_4-15-20.pdf") %>%
+  count(word, sort = TRUE) # 2202
+
+a <- staff_rec  %>%
+  filter(n > 5) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col(fill = "darkred") +
+  xlab(NULL) +
+  ylab("Word Count") +
+  coord_flip() +
+  ggtitle("Staff Recommendation")
+
+b <- proposal  %>%
+  filter(n > 40) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col(fill = "darkblue") +
+  xlab(NULL) +
+  ylab("Word Count") +
+  coord_flip() +
+  ggtitle("Full Proposal")
+
+c <- report  %>%
+  filter(n > 8) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col(fill = "darkgreen") +
+  xlab(NULL) +
+  ylab("Word Count") +
+  coord_flip() +
+  ggtitle("Final Report")
+
+d <- contract  %>%
+  filter(n > 40) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col(fill = "darkorange") +
+  xlab(NULL) +
+  ylab("Word Count") +
+  coord_flip() +
+  ggtitle("Fully Executed")
+
+
+ggarrange(a, b, c, d + rremove("x.text"),
+          labels = c("A", "B", "C", "D"),
+          ncol = 2, nrow = 2)
