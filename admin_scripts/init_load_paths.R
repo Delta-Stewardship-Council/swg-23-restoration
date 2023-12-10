53f92a9bfbc84b942fe9aa5bdbf8ccd26508f8ff
Kenji Tomari <kenjitomari@Kenjis-MacBook-Air.local>
2023-12-10 00:16:42 -0800

    adjusted project initialize script to include a function that loads libraries conditionally, and checks to see if a dir exists, if not creates one.

--- admin_scripts/init_load_paths.R
+++ admin_scripts/init_load_paths.R
@@ -1,19 +1,24 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Load packages
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-lapply(
-  X = c(
-    "stringr",
-    "jsonlite"
-  ),
-  FUN = function(x) {
-    if(!(x %in% .packages())){
-      print(paste0("Loading {", x, "}"))
-      library(x, character.only = T)
+#' Loads libraries from a vector of library names, on the condition they don't 
+#' exist in the environment already.
+#' 
+#' @param .libs character vector
+#' @return NULL
+load_libs <- function(.libs){
+  lapply(
+    X = .libs,
+    FUN = function(lib){
+      if(!(lib %in% .packages()))
+        library(lib, character.only = T) |>
+        suppressMessages() 
     }
-  }
-) |>
-  invisible()
+  ) |>
+    invisible()
+}
+
+load_libs(c("stringr", "jsonlite"))
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Creates file.paths.
@@ -30,3 +35,37 @@ if(!exists("path_data")){
   print("Variable path_data created.")
   path_data <- jsonlite::read_json("paths.json")$box_path
 }
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Function definition: check_dir
+#' Takes a path to file and checks if the directory exists. If not, it creates
+#' the directory. Either way, it informs you of its progress.
+#' 
+#' @param .path character, the file path.
+#' @param .msg logical, whether to deliver a message or not.
+#' @return NULL
+check_dir <- function(.path, 
+                      .msg = T){
+  if(!dir.exists(.path)){
+    msg <- paste0(
+      "Creating path to: ",
+      .path,
+      ".\n"
+    )
+    
+    dir.create(.path)
+  } else {
+    msg <- paste0(
+      "Path, ",
+      .path,
+      ", exists.\n"
+    )
+  }
+  if(.msg){
+    message(msg)
+  }
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Check for a directory, if not make it.
+check_dir(path_data)
